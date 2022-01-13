a4056a41e8cdc8a8a6d40ba1634d2fa8454d72c9
janumohi <97686359+janumohi@users.noreply.github.com>
2022-01-13 21:57:29 +0530

    Update ec2.tf

--- aws/first_demo/ec2.tf
+++ aws/first_demo/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "JanuServer1" {
+resource "aws_instance" "januserver1" {
   ami = "ami-0603cbe34fd08cb81"
   instance_type = "t2.nano"
 
@@ -10,11 +10,11 @@ resource "aws_instance" "JanuServer1" {
 }
 
 output "myawsserver-ip" {
-  value = aws_instance.myawsserver.public_ip
+  value = aws_instance.januserver1.public_ip
 }
 
 output "myserver-PrivateIP" {
-  value = aws_instance.myawsserver.private_ip
+  value = aws_instance.januserver1.private_ip
 }
 
 
